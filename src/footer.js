3afe5f3b1b1cd79b81878f12d7fc32ffa5f264d3
Omar-Hakim Echehamta <hakimasila@gmail.com>
2022-09-10 16:21:40 +0100

    Add year to footer

--- src/footer.js
+++ src/footer.js
@@ -1,13 +1,15 @@
-import { $content, createHtmlElement } from "./index";
+import {createHtmlElement, $content} from "./index";
 
 function render() {
     const $footer = document.createElement("footer");
     const $p = createHtmlElement(
-        "p", null, ["golden"], "Developed by stainslaw"
+        "h4", 
+        null, 
+        ["main-color"], 
+        ["Developed by stainslaw ©" + " " + (new Date().getFullYear())]
     );
-
     $footer.appendChild($p);
     $content.appendChild($footer);
 }
 
-export {render as renderFooter};
+export { render as renderFooter };
