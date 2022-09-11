b47a950c286576b060fae9ae90b957945c90f2ea
Omar-Hakim Echehamta <hakimasila@gmail.com>
2022-09-11 11:09:46 +0100

    Change filler text

--- src/about.js
+++ src/about.js
@@ -20,8 +20,9 @@ function render() {
     const $hr = document.createElement("hr");
 
     const content =
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat, tellus sit amet ultrices porttitor, ex ex aliquet purus, pellentesque rhoncus felis ipsum nec purus. Donec id enim in risus pellentesque blandit. Proin sed nibh nec arcu mollis rhoncus in sit amet augue. Suspendisse magna nisl, eleifend non tempor et, luctus id quam. Nulla bibendum vulputate ante, non malesuada eros lobortis euismod";
-    const $p = createHtmlElement("p", null, ["menu-item"], content);
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat, tellus sit amet ultrices porttitor, ex ex aliquet purus, pellentesque rhoncus felis ipsum nec purus. Donec id enim in risus pellentesque blandit. Proin sed nibh nec arcu mollis rhoncus in sit amet augue. Suspendisse magna nisl, eleifend non tempor et, luctus id quam. Nulla bibendum vulputate ante, non malesuada eros lobortis euismod.";
+        const $p = createHtmlElement("p", null, ["menu-item"], content, "stainslaw");
+
 
     
     $aboutPage.appendChild($cafeTitle);
